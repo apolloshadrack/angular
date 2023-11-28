0097470e967decefb088cb7345601a2482e22d9d
apollo254 <shadrackapollo@gmail.com>
2023-11-28 09:05:11 +0000

    added leave pages

--- src/app/dashboard/leave-details/leave-details.component.ts
+++ src/app/dashboard/leave-details/leave-details.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-leave-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './leave-details.component.html',
   styleUrl: './leave-details.component.scss'
 })
-export class LeaveDetailsComponent {
-
+export class LeaveDetailsComponent implements OnInit {
+    leaveId = '';
+    createdBy = '';
+  constructor(private route: ActivatedRoute){}
+  ngOnInit(): void {
+      this.route.params.subscribe(param =>{
+        this.leaveId = param["id"];
+      })
+  }
 }
--- src/app/dashboard/leave/leave.component.ts
+++ src/app/dashboard/leave/leave.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-leave',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './leave.component.html',
   styleUrl: './leave.component.scss'
 })
