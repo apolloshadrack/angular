647703a36cf75826a6523cd213b2fc3327c5ac4e
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-12-01 16:15:19 +0000

    Add the bar chat

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -1,11 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [CommonModule, NgApexchartsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  dataSource: any;
+  public series: any[] = [
+    {
+      name: 'Requests',
+      data: [44, 55, 57, 56, 61, 58, 63, 60, 66, 68, 78, 23],
+    },
+    {
+      name: 'Attendance',
+      data: [76, 85, 101, 98, 87, 105, 91, 114, 94, 100, 92, 88],
+    },
+  ];
+
+  public chart: any = {
+    type: 'bar',
+    height: 350,
+  };
+
+  public title: any = {
+    text: 'Basic Column Chart',
+    align: 'left',
+  };
 }
