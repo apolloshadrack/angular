84e0bc4de9aa8e2333830af797d6718414f661f7
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-12-02 14:06:10 +0000

    Fix some styling

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -12,12 +12,14 @@ import { NgApexchartsModule } from 'ng-apexcharts';
 export class HomeComponent {
   public series: any[] = [
     {
-      name: 'Requests',
+      name: 'Mest In',
       data: [44, 55, 57, 56, 61, 58, 63, 60, 66, 68, 78, 23],
+      number: 13,
     },
     {
-      name: 'Attendance',
+      name: 'Mest Off',
       data: [76, 85, 101, 98, 87, 105, 91, 114, 94, 100, 92, 88],
+      number: 20,
     },
   ];
 
@@ -31,6 +33,8 @@ export class HomeComponent {
     align: 'left',
   };
 
+  public labels: string[] = this.series.map((s: any) => s.number.toString());
+
   public chartOptions: any = {
     series: [33.3, 33.3, 33.3],
     colors: ['#51BDF9', '#0AA38A', '#2166AC'],
@@ -40,12 +44,13 @@ export class HomeComponent {
     chart: {
       type: 'donut',
     },
-    labels: ['Team A', 'Team B', 'Team C'],
+    // labels: ['Team A', 'Team B', 'Team C'],
     plotOptions: {
       pie: {
         donut: {
           labels: {
-            show: false, // Set to false to hide the numbers
+            show: true,
+            name: 'eits', // Set to false to hide the numbers
           },
         },
       },
