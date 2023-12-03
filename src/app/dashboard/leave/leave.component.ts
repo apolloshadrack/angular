83311c0c9aecd2ae29b0c0289c1fc8a9409ade4b
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-12-03 23:32:43 +0000

    Complete the attendance inner

--- src/app/dashboard/leave/leave.component.ts
+++ src/app/dashboard/leave/leave.component.ts
@@ -3,25 +3,22 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LeaveService } from '../../services/leave.service';
 
-
 @Component({
   selector: 'app-leave',
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './leave.component.html',
-  styleUrl: './leave.component.scss'
+  styleUrl: './leave.component.scss',
 })
 export class LeaveComponent implements OnInit {
-  leaves: any [] = [];
-  constructor(
-    private leaveService: LeaveService
-  ){ }
+  leaves: any[] = [];
+  constructor(private leaveService: LeaveService) {}
   ngOnInit(): void {
     this.getLeaves();
   }
-  getLeaves(){
-    this.leaveService.getLeaves().subscribe((resp : any)=> {
-        this.leaves = resp;
+  getLeaves() {
+    this.leaveService.getLeaves().subscribe((resp: any) => {
+      this.leaves = resp;
     });
   }
 }
